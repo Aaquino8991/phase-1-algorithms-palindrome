3c6d9ddebcaeb165fd7d228c9ae3d9ff1b78eb06
Anthony <aquinoanthony851@gmail.com>
2023-06-07 22:05:37 -0600

    Algortithms Palindrome

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let splitStr = word.split('');
+  let reverseStr = splitStr.reverse();
+  let backwardsWord = reverseStr.join('');
+
+  if (word === backwardsWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+write code that verifies if a word reads the same forwards and backwards.
+  initialize a variable that splits the word into an array
+  initialize a variable that reverses the split array
+  initialize a variable that joins the reversed split array
+  
+  
+display true or false dependant on the return.
+  if word === backwards word
+    display true
+  if not
+    display false
 */
 
 /*
-  Add written explanation of your solution here
+  The process of reversing the word involves the .split(), .reverse(), and .join()
+  methods. The if else block verifies the word passed into the function agianst 
+  the value of the reverseWord variable to determine if they are the same.
 */
 
 // You can run `node index.js` to view these console logs
